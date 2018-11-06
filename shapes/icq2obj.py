46ca57123d2f7988f9b2b22448bdd58892f695ae
Jesse Stone <jstone@psi.edu>
2018-11-06 16:10:39 -0700

    extracted share_edges and share_corners

--- shapes/icq2obj.py
+++ shapes/icq2obj.py
@@ -26,38 +26,8 @@ def main(argv=None):
                 write_vertex(outfile, v[vindex-1])
                 n[i][j][f] = vindex
 
-    for i in range(1, q):
-        n[i][q][6] = n[q-i][q][4]
-        n[i][0][6] = n[i][q][2]
-        n[i][0][5] = n[q][q-i][1]
-        n[i][0][4] = n[q-i][0][1]
-        n[i][0][3] = n[0][i][1]
-        n[i][0][2] = n[i][q][1]
-
-    for j in range(1, q):
-        n[q][j][6] = n[j][q][5]
-        n[q][j][5] = n[0][j][4]
-        n[q][j][4] = n[0][j][3]
-        n[q][j][3] = n[0][j][2]
-        n[0][j][6] = n[q-j][q][3]
-        n[0][j][5] = n[q][j][2]        
-
-    n[0][0][3] = n[0][0][1]
-    n[q][0][4] = n[0][0][1]
-    n[0][0][2] = n[0][q][1]
-    n[q][0][3] = n[0][q][1]
-    n[0][0][4] = n[q][0][1]
-    n[q][0][5] = n[q][0][1]
-    n[0][0][5] = n[q][q][1]
-    n[q][0][2] = n[q][q][1]
-    n[0][0][6] = n[0][q][2]
-    n[q][q][3] = n[0][q][2]
-    n[0][q][5] = n[q][q][2]
-    n[q][0][6] = n[q][q][2]
-    n[q][q][4] = n[0][q][3]
-    n[0][q][6] = n[0][q][3]
-    n[q][q][5] = n[0][q][4]
-    n[q][q][6] = n[0][q][4]
+    share_edges(n, q)
+    share_corners(n, q)
 
     for f in range(1,7):
         for i in range(0,q):
@@ -78,6 +48,43 @@ def main(argv=None):
                 else:
                     write_facet(outfile, n[i][j][f], n[i][j+1][f], n[i+1][j][f])
                     write_facet(outfile, n[i+1][j][f], n[i][j+1][f], n[i+1][j+1][f])
+
+def share_edges(n, q):
+    for i in range(1, q):
+        n[i][q][6] = n[q-i][q][4]
+        n[i][0][6] = n[i][q][2]
+        n[i][0][5] = n[q][q-i][1]
+        n[i][0][4] = n[q-i][0][1]
+        n[i][0][3] = n[0][i][1]
+        n[i][0][2] = n[i][q][1]
+
+    for j in range(1, q):
+        n[q][j][6] = n[j][q][5]
+        n[q][j][5] = n[0][j][4]
+        n[q][j][4] = n[0][j][3]
+        n[q][j][3] = n[0][j][2]
+        n[0][j][6] = n[q-j][q][3]
+        n[0][j][5] = n[q][j][2]        
+
+def share_corners(n, q):
+    n[0][0][3] = n[0][0][1]
+    n[q][0][4] = n[0][0][1]
+    n[0][0][2] = n[0][q][1]
+    n[q][0][3] = n[0][q][1]
+    n[0][0][4] = n[q][0][1]
+    n[q][0][5] = n[q][0][1]
+    n[0][0][5] = n[q][q][1]
+    n[q][0][2] = n[q][q][1]
+    n[0][0][6] = n[0][q][2]
+    n[q][q][3] = n[0][q][2]
+    n[0][q][5] = n[q][q][2]
+    n[q][0][6] = n[q][q][2]
+    n[q][q][4] = n[0][q][3]
+    n[0][q][6] = n[0][q][3]
+    n[q][q][5] = n[0][q][4]
+    n[q][q][6] = n[0][q][4]
+
+
 
 def calc_coordinate(n, v, f, c1, c2):
     i1,j1,v1 = c1
