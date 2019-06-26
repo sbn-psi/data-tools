9098ae529e91e926df2536c34ed201574c5d76a8
Jesse Stone <jstone@psi.edu>
2019-06-26 16:35:51 -0700

    completed script. will now increment the major version of a product.

--- orex/pds4-tools/versioning.py
+++ orex/pds4-tools/versioning.py
@@ -1,4 +1,7 @@
 import re
+import os
+import sys
+from bs4 import BeautifulSoup
 
 LABELFILE_PARSE_VERSIONED_REGEX=r'(.+)_(\d+)_(\d+)\.xml'
 LABELFILE_PARSE_UNVERSIONED_REGEX=r'(.+)\.xml'
@@ -6,56 +9,106 @@ LABELFILE_PARSE_UNVERSIONED_REGEX=r'(.+)\.xml'
 DATAFILE_PARSE_VERSIONED_REGEX=r'(.+)_(\d+)\.([a-z0-9]+)'
 DATAFILE_PARSE_UNVERSIONED_REGEX=r'(.+)\.([a-z0-9]+)'
 
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    filepaths = argv[1:]
 
-def increment_product(labelfile):
-    datafile = extract_datafile(labelfile)
+    for filepath in filepaths:
+        dirname, filename = os.path.split(filepath)
+        increment_product(dirname, filename)
+
+
+def increment_product(path, labelfile):
+    print(labelfile)
+    label = read_label(path, labelfile)
+    datafile = extract_datafile(label)
 
     new_labelfile = increment_labelfile(labelfile)
-    new_datafile = increment_datafile(datafile)
 
-    inject_datafile(labelfile, datafile)
+    if datafile:
+        new_datafile = increment_datafile(datafile)
+        contents = inject_datafile(label, datafile, new_datafile)
+        with open(new_labelfile, "w") as f:
+            f.write(contents)
+        rename(path, datafile, new_datafile)
+    else:
+        rename(path, labelfile, new_labelfile)
 
-    rename(labelfile, new_labelfile)
-    rename(datafile, new_datafile)
+def read_label(path, labelfile):
+    with open(os.path.join(path, labelfile)) as f:
+        return f.read()
 
-def extract_datafile(labelfile):
-    return ''
+def extract_datafile(label):
+    soup = BeautifulSoup(label, 'lxml-xml')
+    if soup.Product_Observational:
+        return extract_observational_datafile(soup.Product_Observational)
+    elif soup.Product_Collection:
+        return extract_collection_datafile(soup.Product_Collection)
+    elif soup.Product_Document:
+        return extract_document_datafile(soup.Product_Document)
+    else:
+        return None
+
+def extract_collection_datafile(product):
+    file_area = product.File_Area_Inventory if product else None
+    file_element = file_area.File if file_area else None
+    file_name = file_element.file_name if file_element else None
+    return file_name.string
+
+def extract_observational_datafile(product):
+    file_area = product.File_Area_Observational if product else None
+    file_element = file_area.File if file_area else None
+    file_name = file_element.file_name if file_element else None
+    return file_name.string
+
+def extract_document_datafile(product):
+    document = product.Document if product else None
+    document_edition = document.Document_Edition if document else None
+    document_file = document_edition.Document_File if document_edition else None
+    file_name = document_file.document_file if document_file else None
+    return file_name.string
 
 def increment_labelfile(labelfile):
     (filebase, major, minor) = parse_labelfile_name(labelfile)
-    newmajor, newminor = major, minor + 1
-    return filebase, newmajor, newminor
+    newmajor, newminor = major + 1 , 0
+    return "{}_{}_{}.xml".format(filebase, newmajor, newminor)
 
 def increment_datafile(datafile):
     (filebase, major, extension) = parse_datafile_name(datafile)
     newmajor = major + 1
-    return (filebase, newmajor, extension)
+    return "{}_{}.{}".format(filebase, newmajor, extension)
 
-def inject_datafile(labelfile, datafile):
-    pass
+def inject_datafile(label, datafile, new_datafile):
+    return label.replace(datafile, new_datafile)
 
-def rename(filename, newfilename):
-    pass
+def rename(dir, filename, newfilename):
+    src = os.path.join(dir, filename)
+    dst = os.path.join(dir, newfilename)
+
+    if (os.path.exists(newfilename)):
+        print ("File already exists: " + newfilename)
+    else:
+        os.rename(src, dst)
 
 def parse_datafile_name(name):
     versioned_match = re.match(DATAFILE_PARSE_VERSIONED_REGEX, name)
     if (versioned_match):
         (filebase, major, extension) = versioned_match.groups()
-        return (filebase, major, extension)
+        return (filebase, int(major), extension)
     else:
         unversioned_match = re.match(DATAFILE_PARSE_UNVERSIONED_REGEX, name)
         (filebase, extension) = unversioned_match.groups()
         return (filebase, 1, extension)
 
-
 def parse_labelfile_name(name):
     versioned_match = re.match(LABELFILE_PARSE_VERSIONED_REGEX, name)
     if (versioned_match):
         (filebase, major, minor) = versioned_match.groups()
-        return (filebase, major, minor)
+        return (filebase, int(major), int(minor))
     else:
         unversioned_match = re.match(LABELFILE_PARSE_UNVERSIONED_REGEX, name)
-        (filebase) = unversioned_match.groups()
+        filebase = unversioned_match.groups()[0]
         return (filebase, 1, 0)
 
 def increment_major(major, minor):
@@ -77,3 +130,6 @@ def attach_version_to_labelfile(filebase, major, minor):
         'minor': minor,
         'filebase': filebase
     })
+
+if __name__ == '__main__':
+    sys.exit(main())
