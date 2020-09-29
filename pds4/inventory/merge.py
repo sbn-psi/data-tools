6410988c128d63da9cf84cfdf15e96b0ae3afa21
Jesse Stone <jstone@psi.edu>
2020-09-29 15:29:48 -0700

    migrated merge code from orex

--- pds4/inventory/merge.py
+++ pds4/inventory/merge.py
@@ -6,6 +6,10 @@ and merging inventories.
 import os
 import argparse
 import sys
+import re
+from bs4 import BeautifulSoup
+
+RECORDS_REGEX=r"\<records\>\d*\<\/records\>"
 
 def main(argv=None):
     if argv is None:
@@ -14,16 +18,34 @@ def main(argv=None):
     parser = argparse.ArgumentParser(description='Merge together PDS4 collection inventory files')
     parser.add_argument('files', nargs='+', help='The inventory files to merge together')
     parser.add_argument('--output', help='The destination file')
+    parser.add_argument('--labelsrc', help='The input label file')
+    parser.add_argument('--labeldest', help='The output label file')
     args = parser.parse_args()
 
     invs = [open(infile).readlines() for infile in args.files]
     merged = invmerge(max, *invs)
     contents = '\r\n'.join(sorted(merged)) + '\r\n'
+
+    records = len(merged)
+    
     if (args.output):
         with open(args.output, 'w') as f:
             f.write(contents)
     else:
         print(contents)
+
+    if args.labelsrc and args.labeldest:
+        update_label_count(args.labelsrc, args.labeldest, records)
+
+
+def update_label_count(labelsrc, labeldest, record_count):
+    with open(labelsrc) as infile:
+        contents = infile.read()
+    new_contents = re.sub(RECORDS_REGEX, "<records>{}</records>".format(record_count), contents)
+    with open(labeldest, "w") as outfile:
+        outfile.write(new_contents)
+    
+    
 
 def invmerge(win_func, *invs):
     '''
