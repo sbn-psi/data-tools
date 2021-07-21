4024307e30067ae59e37b61daaf4e41c53a35a12
Jesse Stone <jstone@psi.edu>
2021-07-21 14:20:38 -0700

    added documentation for each field.

--- orex/ovirs-otes-review/schema.sql
+++ orex/ovirs-otes-review/schema.sql
@@ -9,16 +9,20 @@ Checksum
 */
 
 CREATE TABLE element (
-    instrument text,
-    loc text,
-    proclevel text,
-    filepath text,
-    obsdate text,
-    obsstamp integer,
-    component text,
-    local_id text,
-    sha256sum text
+    instrument text, -- the name of the instrument, such as ovirs or otes
+    loc text, -- the "location" of the data file. I use existing and delivered
+    proclevel text, -- the processing level of the data (raw or calibrated). Could also be used for other things, like geometry
+    filepath text, -- the path of the file relative to the bundle
+    obsdate text, -- the iso date of the observation
+    obsstamp integer, -- the date of the observation, expressed in milliseconds from the epoch
+    component text, -- the type of file segment that was evaluated (Table_Binary, Array_3D, etc)
+    local_id text, -- the local identifier of the segment, if it was provided
+    sha256sum text -- the checksum of the segment
 );
 
+/**
+If necessary, one could add additional metadata for each segment, such as offset and object_length, by modifying this schema and the ingest script.
+*/
 
+-- this index helps match up against similar files more easily
 create index ix_element on element(instrument, loc, proclevel, obsstamp)
