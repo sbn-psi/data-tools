c5959b0dd951f50f560b41c5e5d78fed958283e5
Jesse Stone <jstone@psi.edu>
2024-08-01 16:14:10 -0700

    made output a little more consistent

--- pds4/preflight/main.py
+++ pds4/preflight/main.py
@@ -9,7 +9,7 @@ def main():
     files = sys.argv[1:]
     products = (product.Product(f) for f in files)
     passing = preflight.preflight_products(products)
-    print([p.lidvid() for p in passing])
+    print("\n".join([f"{p.labelfilename} : OK" for p in passing]))
 
 
 if __name__ == "__main__":
