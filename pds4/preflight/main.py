da30166a05dfd38e4fc2c7f0114839b1a056fd30
Jesse Stone <jstone@psi.edu>
2024-09-19 09:55:24 -0700

    Updated to use generators all the way through for file discovery

--- pds4/preflight/main.py
+++ pds4/preflight/main.py
@@ -12,7 +12,7 @@ import product
 
 def main():
     params = sys.argv[1:]
-    files = build_file_list(params)
+    files = build_file_lists(params)
     products = (product.Product(f) for f in files)
     preflights = preflight.preflight_products(products)
     has_warnings = False
@@ -32,21 +32,20 @@ def main():
         return 0
 
 
-def build_file_list(params: Iterable[str]) -> Iterable[str]:
-    result = []
-    for f in params:
-        if os.path.isfile(f) and f.endswith('.xml'):
-            result.append(f)
-        elif os.path.isdir(f):
-            result.extend(
-                itertools.chain.from_iterable(
-                    (os.path.join(dirpath, filename) for filename in filenames if is_product_label(filename))
-                        for dirpath, _, filenames in os.walk(f)
-                )
-            )
-        else:
-            raise Exception(f"Invalid parameter: {f}")
-    return result
+def build_file_lists(params: Iterable[str]) -> Iterable[str]:
+    return itertools.chain.from_iterable(build_file_list(f) for f in params)
+
+
+def build_file_list(f: str) -> Iterable[str]:
+    if os.path.isfile(f) and f.endswith('.xml'):
+        return [f]
+    elif os.path.isdir(f):
+        return itertools.chain.from_iterable(
+            (os.path.join(dirpath, filename) for filename in filenames if is_product_label(filename))
+            for dirpath, _, filenames in os.walk(f)
+        )
+    else:
+        raise Exception(f"Invalid parameter: {f}")
 
 
 def is_product_label(filename):
