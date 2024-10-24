8f079585eeefbff64459f54bbc35bf41badd3e66
Jesse Stone <jstone@psi.edu>
2024-10-24 11:28:00 -0700

    added multiprocessing support. Need to solve logging for this to work

--- pds4/inventory/generate.py
+++ pds4/inventory/generate.py
@@ -4,27 +4,41 @@ import sys
 import inventory
 import argparse
 import logging
+from multiprocessing import pool
 
 def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("outfilepath")
     parser.add_argument("dirname")
     parser.add_argument("--deep-product-check", action='store_true')
+    parser.add_argument("--logfile")
+    parser.add_argument("--processes", type=int, default=1)
 
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(
+        level=logging.INFO,
+        filename=args.logfile if args.logfile else None
+    )
 
-    build_inventory(args.dirname, args.outfilepath, args.deep_product_check)
+    build_inventory(args.dirname, args.outfilepath, args.deep_product_check, args.processes)
 
-def build_inventory(dirname, outfilename, deep):
+def build_inventory(dirname, outfilename, deep, processes):
     filenames = inventory.get_product_filenames(dirname, deep=deep)
-    lidvids = sorted(inventory.iter_extract_lidvid(filename) for filename in filenames)
+    lidvids = sorted(get_lidvids(filenames, processes))
     records = ("P," + lidvid for lidvid in lidvids)
 
     with open(outfilename,"w") as f:
         for r in records:
             f.write(r + "\r\n")
+
+def get_lidvids(filenames, processes):
+    if processes == 1:
+        return (inventory.iter_extract_lidvid(filename) for filename in filenames)
+    else:
+        logging.warning("Logging suppressed during multiprocessing phase")
+        with pool.Pool(processes=processes) as p:
+            return p.map(inventory.iter_extract_lidvid,filenames)
 
 if __name__ == '__main__':
     main()
