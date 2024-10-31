966927a2718c58880af89f2ed4f16f01798b9cae
Jesse Stone <jstone@psi.edu>
2024-10-31 13:55:41 -0700

    fixed most warnings

--- pds4/inventory/inventory.py
+++ pds4/inventory/inventory.py
@@ -9,14 +9,15 @@ except ImportError:
     import xml.etree.ElementTree as etree
 
 logger = logging.getLogger(__name__)
-NON_PRODUCT_FRAGMENTS=('bundle', 'collection')
-NON_PRODUCT_ELEMENTS=('Product_Collection', 'Product_Bundle')
+
+PDS4_NS = "{http://pds.nasa.gov/pds4/pds/v1}"
+NON_PRODUCT_FRAGMENTS = ('bundle', 'collection')
+NON_PRODUCT_ELEMENTS = ('Product_Collection', 'Product_Bundle')
 
 
 def get_all_product_filenames(dirname):
-    return itertools.chain.from_iterable(
-        (os.path.join(path, filename) for filename in filenames) for (path,_,filenames) in os.walk(dirname) if not 'SUPERSEDED' in path
-    )
+    return itertools.chain.from_iterable((os.path.join(path, filename) for filename in filenames)
+                                         for (path, _, filenames) in os.walk(dirname) if 'SUPERSEDED' not in path)
 
 
 def get_product_filenames(dirname, deep):
@@ -33,23 +34,23 @@ def extract_product_type(filename):
     try:
         for (_, elem) in etree.iterparse(filename, events=['start']):
             tag = elem.tag
-            if tag.startswith("{http://pds.nasa.gov/pds4/pds/v1}Product"):
-                return tag.replace("{http://pds.nasa.gov/pds4/pds/v1}","")
+            if tag.startswith(f"{PDS4_NS}Product"):
+                return tag.replace(PDS4_NS, "")
         raise Exception(f"Could not find product type for: {filename}")
     except Exception as e:
         raise Exception(f"Could not parse product: {filename}") from e
 
 
 def iter_extract_lidvid(filename, tolerant=False):
-    lid=""
+    lid = ""
     try:
         for (_, elem) in etree.iterparse(filename):
-            if elem.tag=="{http://pds.nasa.gov/pds4/pds/v1}logical_identifier":
-                lid=elem.text
-            elif elem.tag=="{http://pds.nasa.gov/pds4/pds/v1}version_id":
+            if elem.tag == f"{PDS4_NS}logical_identifier":
+                lid = elem.text
+            elif elem.tag == f"{PDS4_NS}version_id":
                 lidvid = lid + "::" + elem.text
                 return lidvid
-            elif elem.tag=="{http://pds.nasa.gov/pds4/pds/v1}Identification_Area":
+            elif elem.tag == f"{PDS4_NS}Identification_Area":
                 raise Exception(f"Missing LID or VID for: {filename}")
     except Exception as e:
         print(f"Could not parse {filename}: {e}")
@@ -59,16 +60,17 @@ def iter_extract_lidvid(filename, tolerant=False):
 
 
 def inventory_to_dict(inventory):
-    '''
+    """
     Convert a list of inventory lines to a dictionary of LID to (VID, member_type)
     This will fail if there are duplicate LIDs in the inventory
-    '''
+    """
     return dict(_invline_to_tuple(x) for x in inventory)
 
+
 def _invline_to_tuple(invline):
-    '''
+    """
     Convert an inventory line to a tuple of (LID, (VID, member_type))
-    '''
+    """
     member_type, lidvid = [x.strip() for x in invline.split(',')]
     lid, vid = [x.strip() for x in lidvid.split('::')]
-    return (lid, (vid, member_type))
+    return lid, (vid, member_type)
