5289d99367c42338cce3476445144dbfd9b4d98c
Jesse Stone <jstone@psi.edu>
2024-10-31 13:59:04 -0700

    fixed warnings

--- pds4/inventory/validate.py
+++ pds4/inventory/validate.py
@@ -4,19 +4,20 @@ import sys
 import argparse
 import inventory
 
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv
+
+def main():
     parser = argparse.ArgumentParser(description='Validate a PDS4 collection inventory against the directory')
     parser.add_argument('--inventory', help='The inventory file', required=True)
     parser.add_argument('--directory', help='The directory with the products', required=True)
     args = parser.parse_args()
 
     provided_inventory = read_inventory(args.inventory)
-    primary_lidvids = set(k + "::" + provided_inventory[k][0] for k in provided_inventory.keys() if provided_inventory[k][1] == 'P')
-    all_lidvids = set(k + "::" + provided_inventory[k][0] for k in provided_inventory.keys())
+    primary_lidvids = set(k + "::" + provided_inventory[k][0]
+                          for k in provided_inventory.keys() if provided_inventory[k][1] == 'P')
+    all_lidvids = set(k + "::" + provided_inventory[k][0]
+                      for k in provided_inventory.keys())
 
-    filenames = inventory.get_product_filenames(args.directory)
+    filenames = inventory.get_product_filenames(args.directory, False)
     discovered_lidvids = set(inventory.iter_extract_lidvid(filename) for filename in filenames)
 
     print("Discovered, but not in inventory:")
@@ -31,6 +32,7 @@ def main(argv=None):
 def read_inventory(filename):
     with open(filename) as f:
         return inventory.inventory_to_dict(f.readlines())
-    
+
+
 if __name__ == "__main__":
     sys.exit(main())
